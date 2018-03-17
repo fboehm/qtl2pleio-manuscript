cc87060855bf8902a20526b26432c39b378f10b9
Frederick Boehm <frederick.boehm@gmail.com>
2018-03-17 11:57:59 -0500

    added -fix versions of submit files after finding missing jobs on chtc

---
+++ find-missing-jobs.R
@@ -0,0 +1,4 @@
+library(qtl2chtc)
+find_missing_jobs(DIR = "chtc/Recla-bootstrap/results-chtc/boot-run561", outfile = "bad-jobs-run561")
+find_missing_jobs(DIR = "chtc/Recla-bootstrap/results-chtc/boot-run562", outfile = "bad-jobs-run562")
+find_missing_jobs(DIR = "chtc/Recla-bootstrap/results-chtc/boot-run563", outfile = "bad-jobs-run563")
